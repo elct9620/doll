d4c227bd9c7d8810ebebe3f39bdea9f8d4b62e9b
蒼時弦也 <elct9620@frost.tw>
2017-08-29 11:42:32 +0800

    Add simplecov support

--- doll.gemspec
+++ doll.gemspec
@@ -26,4 +26,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.13'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'simplecov'
+  spec.add_development_dependency 'coveralls'
+  spec.add_development_dependency 'codeclimate-test-reporter'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,23 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "doll"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+require 'coveralls'
+require 'codeclimate-test-reporter'
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  )
+
+  load_profile 'test_frameworks'
+
+  add_group 'Adapter', 'lib/doll/adapter'
+  add_group 'NLP', 'lib/doll/nlp'
+  add_group 'Event', 'lib/doll/event'
+  add_group 'Message', 'lib/doll/message'
+end
+
+require 'doll'
